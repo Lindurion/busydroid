477fd45ee3609766660acae325243e129ff2a780
Eric W. Barndollar <eric@EricWBarndollar.com>
2017-01-09 09:38:50 -0800

    Initial EmPubLite Android Studio project

---
+++ EmPubLite/app/src/main/java/com/ericwbarndollar/empublite/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ericwbarndollar.empublite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/** Main activity for the EmPubLite Android app. */
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+}
