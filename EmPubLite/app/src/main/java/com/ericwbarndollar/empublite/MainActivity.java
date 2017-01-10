e164a683d4098f3feed21eff849d33c5524b82fc
Eric W. Barndollar <eric@EricWBarndollar.com>
2017-01-10 15:56:04 -0800

    Through tutorial #7

--- EmPubLite/app/src/main/java/com/ericwbarndollar/empublite/MainActivity.java
+++ EmPubLite/app/src/main/java/com/ericwbarndollar/empublite/MainActivity.java
@@ -2,6 +2,8 @@ package com.ericwbarndollar.empublite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /** Main activity for the EmPubLite Android app. */
 public class MainActivity extends AppCompatActivity {
@@ -12,4 +14,22 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
   }
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.options, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.about:
+        return true;  // Handled.
+
+      case R.id.help:
+        return true;  // Handled.
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
 }
